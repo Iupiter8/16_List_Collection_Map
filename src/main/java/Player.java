9e6484e483b9ab08ba145f9eaa2f73e84f643170
Yurii Kulagin <yuk.178@bk.ru>
2022-10-06 18:28:46 +0300

    New branch

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -19,9 +19,9 @@ public class Player {
 //        this.id = id;
 //    }
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
 //
 //    public void setName(String name) {
 //        this.name = name;
